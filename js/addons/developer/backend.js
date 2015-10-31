15bd9f836aa21bf8b3b99012164e5d52f7759440
WSergio@NZXT <sergiu@disruptive.academy>
2015-10-31 02:47:52 +0200

    Isolated whole structure

---
+++ js/addons/developer/backend.js
@@ -0,0 +1,25 @@
+(function(_, $) {
+
+
+    $(document).ready(function(){
+
+        if (controller == 'addons' && mode == 'manage') {
+            //$('#addons_list .btn-group.dropleft .dropdown-menu').each(function(i, ul) {
+            $('#addons_list .cm-row-item').each(function(i, tr) {
+
+                var $tr = $(tr)
+                    , id = $tr.attr('id')
+                    , addonName = id.replace(/^addon_/, '').replace(/installed/, '')
+                    , $ul = $tr.find('.btn-group.dropleft .dropdown-menu')
+                    , reinstallUrl = fn_url('addons.reinstall?addon=' + addonName)
+                    , packUrl = fn_url('addons.pack?addon=' + addonName)
+                    ;
+                $ul.append('<li class="divider"></li>')
+                    .append('<li><a class="cm-confirm" href="' + reinstallUrl + '">Reinstall</a></li>')
+                    .append('<li><a class="cm-confirm" href="' + packUrl + '">Pack</a></li>');
+            });
+        }
+    });
+
+
+}(Tygh, Tygh.$));
