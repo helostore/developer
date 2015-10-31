e73bf93b179bb4a1e8cfa9efc54db9876ceb56e2
WSergio@NZXT <sergiu@disruptive.academy>
2015-10-31 11:08:14 +0200

    Updated head comment block

--- app/addons/developer/func.php
+++ app/addons/developer/func.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Developer
+ * HELOstore
  *
  * This source file is part of a commercial software. Only users who have purchased a valid license through
  * https://helostore.com/ and accepted to the terms of the License Agreement can install this product.
  *
- * @category   Zend
- * @package    Zend_Application
+ * @category   Add-ons
+ * @package    HELOstore
  * @copyright  Copyright (c) 2015-2016 HELOstore. (https://helostore.com/)
  * @license    https://helostore.com/legal/license-agreement/   License Agreement
  * @version    $Id$
